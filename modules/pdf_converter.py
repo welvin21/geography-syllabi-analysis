55a22ac7ba4afdd474467bc713b9ddc8cad3404e
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-12-28 16:16:06 +0800

    Add scatter plot to visualize trends in temporal dimension (#2)

--- modules/pdf_converter.py
+++ modules/pdf_converter.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[22]:
-
-
 import tika
 import os
 from tika import parser as tika_parser
 tika.initVM()
-
-
-# In[23]:
-
 
 class PDFToText():
     def __init__(self):
@@ -23,10 +13,3 @@ class PDFToText():
         
         parsed_pdf = self.engine.from_file(file_path)
         return parsed_pdf["content"]
-
-
-# In[ ]:
-
-
-
-
